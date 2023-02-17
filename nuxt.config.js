53f097d89e6aa61065be3d0950febd5c835dfea6
ana-wtag <najia.afrin@welldev.io>
2023-02-17 19:44:45 +0600

    merged branch

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~assets/css/main.css'
+    '~assets/css/main.css',
+    '~assets/scss/_variables.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -30,9 +31,15 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/style-resources',
     '@nuxtjs/svg'
   ],
+
+  styleResources: {
+    // scss: [
+    //   '~assets/scss/mixins.scss',
+    //   '~assets/scss/variables.scss'
+    // ]
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/i18n','sweetalert'],
@@ -54,12 +61,7 @@ export default {
     strategy: 'no_prefix'
   },
 
-styleResources: {
-    scss: [
-      '~assets/scss/mixins.scss',
-      '~assets/scss/variables.scss'
-    ]
-  },
+
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
